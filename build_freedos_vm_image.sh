0511779bf9212cc000cfd4ae52f7cd65f77997a8
Volkert de Buisonjé <volkertb@users.noreply.github.com>
2022-07-24 12:16:38 +0200

    Various improvements and changes. See list below:
    * Tweak QEMU parameters
    * Just try out all possible QEMU accelerators instead of relying on OS-specific checks
    * Fall back to TCG (software emulation) when no other accelerators are available
    * Block script on macOS with clear explanation until script has been fixed to work on that OS
    * Scan video buffer for regex patterns instead of exact strings
    * Make timeouts more generous for older systems (particularly those without hardware-assisted virtualization)
    
    * Add timeout mechanisms for startup and shutdown of installation VM instance

--- build_freedos_vm_image.sh
+++ build_freedos_vm_image.sh
@@ -8,6 +8,8 @@ FD_ZIP_FILE=FD13-LiteUSB.zip
 FD_DOWNLOAD_URL=https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.3/official/$FD_ZIP_FILE
 FD_ZIP_FILE_SHA256=64a934585087ccd91a18c55e20ee01f5f6762be712eeaa5f456be543778f9f7e
 FD_DOWNLOAD_PATH=$DOWNLOAD_DIR/$FD_ZIP_FILE
+QEMU_COMMON_PARAMS="-machine pc,accel=kvm:hvf:whpx:xen:hax:nvmm:tcg -smp cpus=1,cores=1 -m 256M -rtc base=localtime
+-no-hpet"
 QEMU_MONITOR_LOCAL_PORT=10789
 VM_DESIRED_IMAGE_SIZE=100M
 
@@ -32,19 +34,17 @@ command -v qemu-system-i386 >/dev/null || {
 # With thanks to https://bugzilla.redhat.com/show_bug.cgi?id=754702#c7
 if nc -q 2>&1 | grep "requires an argument" >/dev/null; then alias nc="nc -q 0"; fi
 
-qemu_accelerator=tcg
 qemu_exec="qemu-system-i386" # qemu-system-i386 is faster for running 16/32 code when TCG (software emulation) is used
 
-# Enable KVM only on Linux systems that support it
-grep -E '^flags.*(vmx|svm)' /proc/cpuinfo >/dev/null 2>&1 && {
-  echo "Linux system detected with KVM available. Using kvm accelerator for hardware-assisted virtualization."
-  qemu_accelerator=kvm
+# FIXME: remove this check once the text mode video buffer checking code has been successfully fixed to work with macOS.
+sw_vers >/dev/null 2>&1 && {
+  echo "Sorry, this script still needs to be fixed to work correctly in macOS. (Hangs in video buffer reading code.)"
+  exit 1
 }
 
 # Enable HVF only on macOS systems that support it
 sysctl kern.hv_support 2>/dev/null | grep "kern.hv_support: 1" >/dev/null && {
-  echo "macOS system detected with HVF available. Using hvf accelerator for hardware-assisted virtualization."
-  qemu_accelerator=hvf
+  echo "macOS system detected with HVF available. Using qemu-system-x86_64 instead of qemu-system-i386."
   # HVF is apparently not available with qemu-system-i386
   qemu_exec="qemu-system-x86_64"
 }
@@ -53,20 +53,20 @@ sysctl kern.hv_support 2>/dev/null | grep "kern.hv_support: 1" >/dev/null && {
 # With thanks to https://unix.stackexchange.com/a/596284
 if grep io_uring_setup /proc/kallsyms >/dev/null 2>&1; then aio_override_prefix="aio=io_uring,"; else aio_override_prefix=""; fi
 
-wait_until_text_mode_shows_string() {
-  string_to_wait_for=$1
+wait_until_text_mode_matches_regex() {
+  regex_to_wait_for=$1
   number_of_retries=$2
   delay_after_each_retry_in_seconds=$3
 
-  echo "Waiting for text mode video buffer to showing string ${string_to_wait_for}"
+  echo "Waiting for text mode video buffer to have contents matching regex ${regex_to_wait_for}"
 
   # With thanks to https://stackoverflow.com/a/34434973 for figuring out a POSIX-compliant while-loop
   j=0
   while [ $j -le "${number_of_retries}" ]; do
     j=$((j + 1))
-    echo "Checking if text mode video buffer is showing string ${string_to_wait_for}, attempt $j"
+    echo "Checking if text mode video buffer is matching regex ${regex_to_wait_for}, attempt $j"
 
-    echo "pmemsave 0xb8000 4000 \"/tmp/videodumpbuffer\"" | nc localhost $QEMU_MONITOR_LOCAL_PORT &
+    echo "pmemsave 0xb8000 4000 \"/tmp/videodumpbuffer\"" | nc localhost $QEMU_MONITOR_LOCAL_PORT >/dev/null &
 
     # With thanks to https://stackoverflow.com/a/58837696 (for stripping out every second byte from the video dump)
     (LC_ALL=C awk -F "" '{
@@ -75,11 +75,11 @@ wait_until_text_mode_shows_string() {
                     printf $i
             if(p=((p&&NF%2)||(!p&&!(NF%2))))  # xor
                 printf "\n"                   # handle newlines, all but the last
-        }' /tmp/videodumpbuffer | grep "$string_to_wait_for") && return 0
+        }' /tmp/videodumpbuffer | grep -E "$regex_to_wait_for") && return 0
 
     sleep "${delay_after_each_retry_in_seconds}"s
   done
-  echo "Timeout after waiting too long for text mode video buffer to show string ${string_to_wait_for}" >/dev/stderr
+  echo "Timeout after waiting too long for text mode video buffer to match regex ${regex_to_wait_for}" >/dev/stderr
   return 1
 }
 
@@ -104,10 +104,9 @@ cd $BUILD_DIR
 rm -rf ./*
 unzip ../$FD_DOWNLOAD_PATH
 qemu-img create -f qcow2 freedos.qcow2 $VM_DESIRED_IMAGE_SIZE
-echo "QEMU accelerator to be used: $qemu_accelerator"
+# shellcheck disable=SC2086
 $qemu_exec \
-  -accel $qemu_accelerator \
-  -machine pc \
+  $QEMU_COMMON_PARAMS \
   -drive "${aio_override_prefix}"if=virtio,format=raw,file=FD13LITE.img \
   -drive "${aio_override_prefix}"if=virtio,format=qcow2,file=freedos.qcow2 \
   -drive if=none,id=drive-ide0-0-0,readonly=on \
@@ -118,22 +117,28 @@ echo "QEMU launched, PID: $qemu_pid"
 
 echo Waiting for QEMU instance to spin up
 # With thanks to https://stackoverflow.com/a/27601038
+qemu_startup_wait_retries=20
 while ! nc -z localhost $QEMU_MONITOR_LOCAL_PORT; do
+  [ "$qemu_startup_wait_retries" -eq 0 ] && {
+    echo 'Timeout waiting for QEMU to start up.'
+    exit 1
+  }
+  qemu_startup_wait_retries="$((qemu_startup_wait_retries - 1))"
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 echo "QEMU monitor port is listening."
 
-wait_until_text_mode_shows_string "What is your preferred language?" 10 1
+wait_until_text_mode_matches_regex "What is your preferred language\?" 100 1
 
 echo Pressing ENTER for English
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Do you want to proceed?" 5 1
+wait_until_text_mode_matches_regex "Do you want to proceed\?" 30 1
 
 echo Pressing ENTER for "Yes - Continue with the installation"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Yes - Partition drive D:" 5 1
+wait_until_text_mode_matches_regex "Yes \- Partition drive D:" 30 1
 
 echo ARROW-UP FOR "Yes - Partition drive D:"
 echo sendkey up | nc localhost $QEMU_MONITOR_LOCAL_PORT
@@ -144,23 +149,23 @@ echo ENTER FOR "Yes - Partition drive D:"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
 echo Waiting for drive to be partitioned
-wait_until_text_mode_shows_string "Yes - Please reboot now" 10 1
+wait_until_text_mode_matches_regex "Yes \- Please reboot now" 60 1
 
 echo ENTER FOR "Yes - Please reboot now"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
 echo Waiting for reboot
-wait_until_text_mode_shows_string "What is your preferred language?" 10 1
+wait_until_text_mode_matches_regex "What is your preferred language\?" 100 1
 
 echo ENTER for English
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Yes - Continue with the installation" 5 1
+wait_until_text_mode_matches_regex "Yes \- Continue with the installation" 30 1
 
 echo ENTER for "Yes - Continue with the installation"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Yes - Please erase and format drive D:" 5 1
+wait_until_text_mode_matches_regex "Yes \- Please erase and format drive D:" 30 1
 
 echo ARROW-UP FOR "Yes - Please erase and format drive D:"
 echo sendkey up | nc localhost $QEMU_MONITOR_LOCAL_PORT
@@ -170,23 +175,23 @@ sleep 1s
 echo ENTER FOR "Yes - Please erase and format drive D:"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Press a key..." 5 1
+wait_until_text_mode_matches_regex "Press a key\.\.\." 30 1
 
 echo ENTER for "press a key (skip countdown)"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
 echo Wait for "Gathering some information(...)"
-wait_until_text_mode_shows_string "Please select your keyboard layout." 5 1
+wait_until_text_mode_matches_regex "Please select your keyboard layout\." 30 1
 
 echo ENTER for "US English (Default)"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Plain DOS system" 5 1
+wait_until_text_mode_matches_regex "Plain DOS system" 30 1
 
 echo ENTER for "Plain DOS system"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "Yes - Please install FreeDOS" 5 1
+wait_until_text_mode_matches_regex "Yes \- Please install FreeDOS" 30 1
 
 echo ARROW-UP FOR "Yes - Please install FreeDOS"
 echo sendkey up | nc localhost $QEMU_MONITOR_LOCAL_PORT
@@ -196,10 +201,8 @@ sleep 1s
 echo ENTER FOR "Yes - Please install FreeDOS"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-wait_until_text_mode_shows_string "10%" 30 5
-
 echo Waiting for all files to be copied
-wait_until_text_mode_shows_string "Do you want to reboot now?" 30 5
+wait_until_text_mode_matches_regex "Do you want to reboot now\?" 360 5
 
 echo ARROW-DOWN FOR "No  - Return to DOS"
 echo sendkey down | nc localhost $QEMU_MONITOR_LOCAL_PORT
@@ -210,7 +213,7 @@ echo ENTER FOR "No  - Return to DOS"
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
 # We need four backslashes for escaping a single backslash. See https://stackoverflow.com/a/38584332
-wait_until_text_mode_shows_string ":\\\\>" 5 1
+wait_until_text_mode_matches_regex ":\\\\>" 30 1
 
 echo Entering "HALT" and pressing ENTER to shut down the VM
 echo sendkey h | nc localhost $QEMU_MONITOR_LOCAL_PORT # Halt
@@ -219,24 +222,32 @@ echo sendkey l | nc localhost $QEMU_MONITOR_LOCAL_PORT # haLt
 echo sendkey t | nc localhost $QEMU_MONITOR_LOCAL_PORT # halT
 echo sendkey ret | nc localhost $QEMU_MONITOR_LOCAL_PORT
 
-echo Wait for the installation VM to spin down
+echo "Remove temporary FIFO buffer"
+rm /tmp/videodumpbuffer
 
+echo "Wait for the installation VM to spin down"
+
+qemu_shutdown_wait_retries=20
 while kill -0 $qemu_pid >/dev/null 2>&1; do
+  [ "$qemu_shutdown_wait_retries" -eq 0 ] && {
+    echo 'Timeout waiting for QEMU to shut down.'
+    exit 1
+  }
+  qemu_shutdown_wait_retries="$((qemu_shutdown_wait_retries - 1))"
   echo "Waiting for QEMU process to end.."
   sleep 1
 done
 
-echo "FreeDOS VM created. Spinning it up, to you can try it out. Enter the command \"halt\" to shut it down."
-$qemu_exec \
-  -machine pc \
-  -drive "${aio_override_prefix}"if=virtio,format=qcow2,file=freedos.qcow2 \
-  -drive if=none,id=drive-ide0-0-0,readonly=on &
-
+echo "Clean up temporary files"
 rm FD13LITE.*
 rm readme.txt
 
-rm /tmp/videodumpbuffer
+echo "FreeDOS VM created. Spinning it up, to you can try it out. Enter the command \"halt\" to shut it down.
+Have fun! :)"
+# shellcheck disable=SC2086
+$qemu_exec \
+  $QEMU_COMMON_PARAMS \
+  -drive "${aio_override_prefix}"if=virtio,format=qcow2,file=freedos.qcow2 \
+  -drive if=none,id=drive-ide0-0-0,readonly=on &
 
 cd ..
-
-echo "Creation job completed. Have fun! :)"
